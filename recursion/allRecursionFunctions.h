27ec8ae22de537cd82e7acf9a57768310b1d2cba
imsarang <84568747+imsarang@users.noreply.github.com>
2023-01-20 12:43:17 +0530

    subsets and keypad

--- recursion/allRecursionFunctions.h
+++ recursion/allRecursionFunctions.h
@@ -54,3 +54,91 @@ void removeX(char input[])
     }
     return removeX(input + 1);
 }
+
+// return all String subsequences 
+int stringSubSeq(string input,string output[]){
+    
+    // Base Condition
+    if(input.empty()){
+        output[0] = " ";
+        return 1;
+    }
+
+    string smallString = input.substr(1);
+
+    // Recursive Call
+    int smallOutputSize = stringSubSeq(smallString,output);
+    for(int i = 0;i<smallOutputSize;i++){
+        output[i+smallOutputSize] = input[0] + output[i];
+    }
+    return 2*smallOutputSize;
+}
+
+// return keypad
+int keypad(int num,string output[]){
+    // Base Condition
+    if(num==0){
+        output[0] = " ";
+        return 1;
+    }
+
+    string input;
+
+    // Recursive Call
+    int smallOutputSize = keypad(num/10,output);
+
+    // assign string based on the num%10 single digit number
+    switch(num%10){
+        case 2: input = "abc";break;
+        case 3: input = "def";break;
+        case 4: input = "ghi";break;
+        case 5: input = "jkl";break;
+        case 6: input = "mno";break;
+        case 7: input = "pqrs";break;
+        case 8: input = "tuv";break;
+        case 9: input = "wxyz";break;
+    }
+
+    // total solution size will be the already existing output size from recursion times the length of the current input string calculated in the switch case
+    int ans_size = smallOutputSize * input.size();
+    int k = 0;
+    string temp[ans_size];
+
+    // output consists results from previous recursion steps and input consists the current string
+    // the output array is copied the same number of times as the length of input string from switch case
+
+    for(int i = 0;i<smallOutputSize;i++){
+        for(int j = 0;j<input.size();j++){
+            temp[k] = output[i] + input[j];
+            k++; 
+        }
+    }
+
+    // copying elements from temp to output array
+    for(int i = 0;i<ans_size;i++) output[i] = temp[i];
+
+    // returning the size of the output array
+    return ans_size;
+}
+
+
+// Return subsets
+int subsets(int input[],int n,int output[][20]){
+    // Base Condition
+    if(n<=0){
+        output[0][0] = 0;
+        return 1;
+    }
+
+    // Recursive call
+    int count = subsets(input+1,n-1,output);
+    for(int i=0;i<count;i++){
+        int col = output[i][0] + 1;
+        output[i+count][0] = col;
+        output[i+count][1] = input[0];
+        for(int j = 2;j<col;j++){
+            output[count+i][j] = output[i][j-1];
+        }
+    }
+    return 2 * count;
+}
--- recursion/index.cpp
+++ recursion/index.cpp
@@ -6,6 +6,54 @@
 #include "./allSortingFuctions.h"
 
 using namespace std;
+
+// ALL printing functions here
+void keypadHelper(int num,string output,string options[10]){
+    if(num==0){
+        cout<<output<<endl;
+        return ;
+    }
+
+    int digit = num%10;
+    // taking one character at a time inside a for loop : number of iterations = size of the options[digit] string
+    for(int i = 0;i<options[digit].length();i++){
+        keypadHelper(num/10,options[digit][i]+output,options);
+    }
+    return;
+}
+void printKeypad(int num){
+    string options[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    keypadHelper(num,"",options);
+}
+
+void printSubsetHelper(int input[],int n,int output[],int outputSize){
+    
+    // Base Condition
+    if(n==0){
+        for(int i = 0;i<outputSize;i++){
+            cout<<output[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+
+    // Recursive Calls
+    printSubsetHelper(input+1,n-1,output,outputSize);
+
+    int newOutput[20] = {};
+    for(int i = 0;i<outputSize;i++){
+        newOutput[i] = output[i];
+    }
+    newOutput[outputSize] = input[0];
+    printSubsetHelper(input+1,n-1,newOutput,outputSize+1);
+
+}
+
+void printSubsets(int input[],int n){
+    int ans[20] = {};
+    printSubsetHelper(input,n,ans,0);
+}
+
 int main()
 {
     //char input[1000];
@@ -19,11 +67,32 @@ int main()
     // }
     //removeX(input);
     // cout<<input;
-    int input[] = {9,3,7,2,1,5,6};
+
+    // int input[] = {9,3,7,2,1,5,6};
     // mergeSort(input,7);
-    quickSort(input,7);
-    for(auto i : input){
-        cout<<i<<" ";
-    }
+    // quickSort(input,7);
+    // for(auto i : input){
+    //     cout<<i<<" ";
+    // }
+
+    string input = "abc";
+    // string *output = new string[1000];
+    // int outputSize = stringSubSeq(input,output);
+    // int outputSize = keypad(23,output);
+    // cout<<outputSize;
+    // for(int i = 0;i<outputSize;i++){
+    //     cout<<output[i]<<endl;
+    // }
+    // printKeypad(23);
+    int outputSubset[1000][20];
+    int inputSet[3] = {15,12,20};
+    // int subsetOutput = subsets(inputSet,3,outputSubset);
+    // for(int i= 0;i<subsetOutput;i++){
+    //     for(int j =1;j<=outputSubset[i][0];j++){
+    //         cout<<outputSubset[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    printSubsets(inputSet,3);
     return 0;
 }
