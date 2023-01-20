ac4e5e897e8f00a7a0147e9888d5064aef648d05
imsarang <84568747+imsarang@users.noreply.github.com>
2023-01-20 23:52:01 +0530

    subsets

--- recursion/allRecursionFunctions.h
+++ recursion/allRecursionFunctions.h
@@ -55,11 +55,13 @@ void removeX(char input[])
     return removeX(input + 1);
 }
 
-// return all String subsequences 
-int stringSubSeq(string input,string output[]){
-    
+// return all String subsequences
+int stringSubSeq(string input, string output[])
+{
+
     // Base Condition
-    if(input.empty()){
+    if (input.empty())
+    {
         output[0] = " ";
         return 1;
     }
@@ -67,17 +69,20 @@ int stringSubSeq(string input,string output[]){
     string smallString = input.substr(1);
 
     // Recursive Call
-    int smallOutputSize = stringSubSeq(smallString,output);
-    for(int i = 0;i<smallOutputSize;i++){
-        output[i+smallOutputSize] = input[0] + output[i];
+    int smallOutputSize = stringSubSeq(smallString, output);
+    for (int i = 0; i < smallOutputSize; i++)
+    {
+        output[i + smallOutputSize] = input[0] + output[i];
     }
-    return 2*smallOutputSize;
+    return 2 * smallOutputSize;
 }
 
 // return keypad
-int keypad(int num,string output[]){
+int keypad(int num, string output[])
+{
     // Base Condition
-    if(num==0){
+    if (num == 0)
+    {
         output[0] = " ";
         return 1;
     }
@@ -85,18 +90,35 @@ int keypad(int num,string output[]){
     string input;
 
     // Recursive Call
-    int smallOutputSize = keypad(num/10,output);
+    int smallOutputSize = keypad(num / 10, output);
 
     // assign string based on the num%10 single digit number
-    switch(num%10){
-        case 2: input = "abc";break;
-        case 3: input = "def";break;
-        case 4: input = "ghi";break;
-        case 5: input = "jkl";break;
-        case 6: input = "mno";break;
-        case 7: input = "pqrs";break;
-        case 8: input = "tuv";break;
-        case 9: input = "wxyz";break;
+    switch (num % 10)
+    {
+    case 2:
+        input = "abc";
+        break;
+    case 3:
+        input = "def";
+        break;
+    case 4:
+        input = "ghi";
+        break;
+    case 5:
+        input = "jkl";
+        break;
+    case 6:
+        input = "mno";
+        break;
+    case 7:
+        input = "pqrs";
+        break;
+    case 8:
+        input = "tuv";
+        break;
+    case 9:
+        input = "wxyz";
+        break;
     }
 
     // total solution size will be the already existing output size from recursion times the length of the current input string calculated in the switch case
@@ -107,38 +129,133 @@ int keypad(int num,string output[]){
     // output consists results from previous recursion steps and input consists the current string
     // the output array is copied the same number of times as the length of input string from switch case
 
-    for(int i = 0;i<smallOutputSize;i++){
-        for(int j = 0;j<input.size();j++){
+    for (int i = 0; i < smallOutputSize; i++)
+    {
+        for (int j = 0; j < input.size(); j++)
+        {
             temp[k] = output[i] + input[j];
-            k++; 
+            k++;
         }
     }
 
     // copying elements from temp to output array
-    for(int i = 0;i<ans_size;i++) output[i] = temp[i];
+    for (int i = 0; i < ans_size; i++)
+        output[i] = temp[i];
 
     // returning the size of the output array
     return ans_size;
 }
 
-
 // Return subsets
-int subsets(int input[],int n,int output[][20]){
+int subsets(int input[], int n, int output[][20])
+{
     // Base Condition
-    if(n<=0){
+    if (n <= 0)
+    {
         output[0][0] = 0;
         return 1;
     }
 
     // Recursive call
-    int count = subsets(input+1,n-1,output);
-    for(int i=0;i<count;i++){
+    int count = subsets(input + 1, n - 1, output);
+    for (int i = 0; i < count; i++)
+    {
         int col = output[i][0] + 1;
-        output[i+count][0] = col;
-        output[i+count][1] = input[0];
-        for(int j = 2;j<col;j++){
-            output[count+i][j] = output[i][j-1];
+        output[i + count][0] = col;
+        output[i + count][1] = input[0];
+        for (int j = 2; j < col; j++)
+        {
+            output[count + i][j] = output[i][j - 1];
         }
     }
     return 2 * count;
+}
+
+// Return all permutations
+int permutations(string input, string output[])
+{
+    // Base Condition
+    if (input.length() == 0)
+    {
+        output[0] = "";
+        return 1;
+    }
+
+    string smallOutput[10000];
+    // Recursive Call
+    int smallOutputSize = permutations(input.substr(1), smallOutput);
+    int k = 0;
+    for (int i = 0; i < smallOutputSize; i++)
+    {
+        for (int j = 0; j <= smallOutput[i].length(); j++)
+        {
+            output[k++] = smallOutput[i].substr(0, j) + input[0] + smallOutput[i].substr(j);
+        }
+    }
+    return k;
+}
+
+// Return all subsets with sum equal to k
+// output array : 2D array
+// first element of each row = size of the subset
+// second element onwards => elements in the subset
+int getSubsetKHelper(int input[], int n, int startIndex, int output[][50], int k)
+{
+
+    // Base Condition
+    if (startIndex == n)
+    {
+        if (k == 0)
+        {
+            output[0][0] = 0;
+            return 1;
+        }
+        else
+            return 0;
+    }
+
+    int smallOutput1[1000][50], smallOutput2[1000][50];
+
+    // Recursion calls
+    
+    // donot include the current first element of the input array
+    int size1 = getSubsetKHelper(input, n, startIndex + 1, smallOutput1, k);
+    
+    // include the current first element of the input array
+    int size2 = getSubsetKHelper(input, n, startIndex + 1, smallOutput2, k - input[0]);
+
+    int row = 0;
+
+    // first take all the subsets without current first element of the input array
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < smallOutput1[i][0]; i++)
+        {
+            output[row][j] = smallOutput1[i][j];
+        }
+        row++;
+    }
+
+    // take all the subsets with current first element of the array with k=0
+    for (int i = 0; i < size2; i++)
+    {
+        // first element should be size of the subset
+        output[row][0] = smallOutput2[i][0] + 1;
+
+        // second element should be the current first element i.e element at startIndex
+        output[row][1] = input[startIndex];
+
+        // copy all the elements from the smallOutput2 to the main output 2-D array from j=2
+        for (int j = 1; j < smallOutput2[i][0]; j++)
+        {
+            output[row][j+1] = smallOutput2[i][j];
+        }
+        row++;
+    }
+    return row;
+}
+
+int getSubsetsK(int input[], int n, int output[][50], int k)
+{
+    return getSubsetKHelper(input, n, 0, output, k);
 }
--- recursion/index.cpp
+++ recursion/index.cpp
@@ -8,91 +8,148 @@
 using namespace std;
 
 // ALL printing functions here
-void keypadHelper(int num,string output,string options[10]){
-    if(num==0){
-        cout<<output<<endl;
-        return ;
+
+// Print keypad problem
+void keypadHelper(int num, string output, string options[10])
+{
+    if (num == 0)
+    {
+        cout << output << endl;
+        return;
     }
 
-    int digit = num%10;
+    int digit = num % 10;
     // taking one character at a time inside a for loop : number of iterations = size of the options[digit] string
-    for(int i = 0;i<options[digit].length();i++){
-        keypadHelper(num/10,options[digit][i]+output,options);
+    for (int i = 0; i < options[digit].length(); i++)
+    {
+        keypadHelper(num / 10, options[digit][i] + output, options);
     }
     return;
 }
-void printKeypad(int num){
-    string options[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    keypadHelper(num,"",options);
+void printKeypad(int num)
+{
+    string options[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    keypadHelper(num, "", options);
 }
 
-void printSubsetHelper(int input[],int n,int output[],int outputSize){
-    
+// Print subsets problem
+void printSubsetHelper(int input[], int n, int output[], int outputSize)
+{
+
     // Base Condition
-    if(n==0){
-        for(int i = 0;i<outputSize;i++){
-            cout<<output[i]<<" ";
+    if (n == 0)
+    {
+        for (int i = 0; i < outputSize; i++)
+        {
+            cout << output[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
         return;
     }
 
     // Recursive Calls
-    printSubsetHelper(input+1,n-1,output,outputSize);
+    printSubsetHelper(input + 1, n - 1, output, outputSize);
 
     int newOutput[20] = {};
-    for(int i = 0;i<outputSize;i++){
+    for (int i = 0; i < outputSize; i++)
+    {
         newOutput[i] = output[i];
     }
     newOutput[outputSize] = input[0];
-    printSubsetHelper(input+1,n-1,newOutput,outputSize+1);
-
+    printSubsetHelper(input + 1, n - 1, newOutput, outputSize + 1);
 }
 
-void printSubsets(int input[],int n){
+void printSubsets(int input[], int n)
+{
     int ans[20] = {};
-    printSubsetHelper(input,n,ans,0);
+    printSubsetHelper(input, n, ans, 0);
+}
+
+// Print permutations
+void permutationHelper(string input, string output)
+{
+
+    // Base Condition
+    if (input.length() == 0)
+    {
+        cout << output << endl;
+        return;
+    }
+
+    // Recursion call
+    for (int i = 0; i < output.length(); i++)
+    {
+        permutationHelper(input.substr(1), output.substr(0, i) + input[0] + output.substr(i));
+    }
+}
+
+void permutationPrint(string input)
+{
+    // string output="";
+    permutationHelper(input, "");
 }
 
 int main()
 {
-    //char input[1000];
-    //cin >> input;
-    // cout << checkPalindrome(input);
-    // vector<pair<int, int>> output;
-    // TowerOfHanoi(3, 1, 3, 2, output);
-    // for (int i = 0;i<output.size();i++)
-    // {
-    //     cout << "Source : " << output[i].first << " Destination : " << output[i].second << endl;
-    // }
-    //removeX(input);
-    // cout<<input;
+    /*char input[1000];
+    cin >> input;
+    cout << checkPalindrome(input);
+    vector<pair<int, int>> output;
+    TowerOfHanoi(3, 1, 3, 2, output);
+    for (int i = 0; i < output.size(); i++)
+    {
+        cout << "Source : " << output[i].first << " Destination : " << output[i].second << endl;
+    }
+    removeX(input);
+    cout << input;*/ 
 
-    // int input[] = {9,3,7,2,1,5,6};
-    // mergeSort(input,7);
-    // quickSort(input,7);
-    // for(auto i : input){
-    //     cout<<i<<" ";
-    // }
+    /*int input[] = {9,3,7,2,1,5,6};
+    mergeSort(input,7);
+    quickSort(input,7);
+    for(auto i : input){
+        cout<<i<<" ";
+    }*/
 
-    string input = "abc";
-    // string *output = new string[1000];
-    // int outputSize = stringSubSeq(input,output);
-    // int outputSize = keypad(23,output);
-    // cout<<outputSize;
-    // for(int i = 0;i<outputSize;i++){
-    //     cout<<output[i]<<endl;
-    // }
-    // printKeypad(23);
-    int outputSubset[1000][20];
-    int inputSet[3] = {15,12,20};
-    // int subsetOutput = subsets(inputSet,3,outputSubset);
-    // for(int i= 0;i<subsetOutput;i++){
-    //     for(int j =1;j<=outputSubset[i][0];j++){
-    //         cout<<outputSubset[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-    printSubsets(inputSet,3);
+    /*string input = "abc";
+    string *output = new string[1000];
+    int outputSize = stringSubSeq(input, output);
+    int outputSize = keypad(23, output);
+    cout << outputSize;
+    for (int i = 0; i < outputSize; i++)
+    {
+        cout << output[i] << endl;
+    }*/
+
+    /* printKeypad(23);*/
+
+    /*int outputSubset[1000][20];
+    int inputSet[3] = {15, 12, 20};
+    int subsetOutput = subsets(inputSet, 3, outputSubset);
+    for (int i = 0; i < subsetOutput; i++)
+    {
+        for (int j = 1; j <= outputSubset[i][0]; j++)
+        {
+            cout << outputSubset[i][j] << " ";
+        }
+        cout << endl;
+    }*/
+
+    /*printSubsets(inputSet,3);*/
+
+    /*string output[10000];
+    int size = permutations("abcd",output);
+    for(int i = 0;i<size;i++){
+        cout<<output[i]<<endl;
+    }*/
+
+    int input[9] = {5,12,3,17,1,18,15,3,17},output[1000][50];
+    int size = getSubsetsK(input,9,output,6);
+    cout<<size;
+    for(int i = 0;i<size;i++){
+        for(int j = 1;j<output[i][0];j++){
+            cout<<output[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
